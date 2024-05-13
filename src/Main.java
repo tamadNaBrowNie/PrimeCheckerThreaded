0e5413477486a7e4393be8a083dbd5c3d09534b6
tamadNaBrowNie <shawn_aberin@dlsu.edu.ph>
2024-05-13 15:54:35 +0800

    finally prompts user for input

--- src/Main.java
+++ src/Main.java
@@ -26,6 +26,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
+        System.out.println("Enter input and exponent of 2 for thread count");
         read();
         Instant start = Instant.now();
         Threader[] threads = new Threader[thread_count];
