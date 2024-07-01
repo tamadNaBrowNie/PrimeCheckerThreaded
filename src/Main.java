45b0f5a2869dfdba78242c102f401f2b520c4bd9
tamadNaBrowNie <shawn_aberin@dlsu.edu.ph>
2024-07-01 15:40:15 +0800

    seperating inputs from processing

--- src/Main.java
+++ src/Main.java
@@ -9,24 +9,18 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.StringJoiner;
 import java.io.FileOutputStream;
 
+// THIS IS THE SLAVE PROCESS
 public class Main {
-    private static final int LIMIT = 10000000;
-    private static int input = LIMIT;
-    private static BufferedReader buf_in = new BufferedReader(
-            new InputStreamReader(System.in));
-    private static OutputStream buf_so = new BufferedOutputStream(System.out, 1 << 16);
-    private static int thread_count = 1;
-
-    private static int getInput(String msg) throws IOException {
+    private static int getInput(BufferedReader in, OutputStream out, String msg) throws IOException {
 
         try {
-            buf_so.write(msg.getBytes());
-            buf_so.flush();
-            int ans = Integer.parseInt(buf_in.readLine());
+            out.write(msg.getBytes());
+            out.flush();
+            int ans = Integer.parseInt(in.readLine());
 
             return ans;
         } catch (NumberFormatException f) {
@@ -35,96 +29,144 @@ public class Main {
 
         }
         // buf_in.readLine();
-        return getInput(msg);
+        return getInput(in, out, msg);
     }
 
-    private static String getString(String msg) throws IOException {
+    private static String getString(BufferedReader in, String msg) throws IOException {
 
-        buf_so.write(msg.getBytes());
-        buf_so.flush();
-        return buf_in.readLine();
+        System.out.println(msg);
+        return in.readLine();
 
     }
 
-    private static void read() throws IOException {
-        int pow = -1;
+    private static int getInput(BufferedReader buf_in, OutputStream buf_so) throws IOException {
+        int in = 0;
+        final int LIMIT = 100000000;
         do {
-            Main.input = getInput("\nEnter number to search: ");
+            in = getInput(buf_in, buf_so, "\nEnter number to search: ");
 
-            if (Main.input > LIMIT)
+            if (in > LIMIT)
                 buf_so.write("Input too large. enter again\n".getBytes());
 
-            else if (Main.input < 2)
+            else if (in < 2)
                 buf_so.write("Input too small. enter again\n".getBytes());
-        } while (Main.input < 2 || Main.input > LIMIT);
+        } while (in < 2 || in > LIMIT);
+        return in;
+    }
 
+    private static int getPow(BufferedReader buf_in, OutputStream buf_so) throws IOException {
+        int pow = -1;
         while (pow < 0 || pow > 10) {
 
-            pow = getInput("Core counts is 2^n where 0 <= n < 11 and n is your input: ");
+            pow = getInput(buf_in, buf_so, "\nCore counts is 2^n where 0 <= n < 11 and n is your input: ");
             if (pow < 0 || pow > 10)
                 buf_so.write("Invalid power".getBytes());
         }
-        Main.thread_count = 1 << pow;
-
+        return 1 << pow;
     }
 
-    private static final String fString = "\n%d primes were found. %d threads took %.3f ms \n";
     private static final String CYKA = "I/O SNAFU";
 
     public static void main(String[] args) {
 
-        boolean scripted = false;
+        try (BufferedReader buf_in = new BufferedReader(
+                new InputStreamReader(System.in))) {
+            OutputStream buf_so = new BufferedOutputStream(System.out, 1 << 16);
+            boolean scripted = false;
 
-        try {
-            scripted = getInput("Automate? 0 for no, else yes") != 0;
-            if (scripted) {
-                String where = getString("Write where?");
+            try {
+                scripted = getInput(buf_in, buf_so, "Automate? 0 for no, else yes") != 0;
+                if (scripted) {
+                    String where = getString(buf_in, "Write where?");
+                    buf_in.close();
+                    buf_so.close();
+                    buf_so = new BufferedOutputStream(new FileOutputStream(where));
+                    // buf_so.write(getResults().getBytes());
+                    String str = getResults();
+                    buf_so.write(str.getBytes());
+                    buf_so.flush();
+                    buf_so.close();
+
+                    return;
+                }
+                int input = getInput(buf_in, buf_so);
+                int thread_count = getPow(buf_in, buf_so);
                 buf_in.close();
-                buf_so.close();
-                buf_so = new BufferedOutputStream(new FileOutputStream(where));
-                getResults();
+                String str = doTask(input, thread_count);
+                try {
+                    buf_so.write(str.getBytes());
+
+                } catch (IOException e) {
+                    System.out.println(CYKA);
+                    System.err.println(CYKA + " when getting input");
+                    e.printStackTrace();
+                }
                 buf_so.flush();
                 buf_so.close();
 
-                return;
+            } catch (IOException e) {
+                System.out.println(CYKA);
+                System.err.println(CYKA + " when getting input");
             }
-            read();
-            buf_in.close();
-            doTask();
-            buf_so.flush();
-            buf_so.close();
-
         } catch (IOException e) {
-            System.out.println(CYKA);
-            System.err.println(CYKA + " when getting input");
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
 
     }
 
-    private static void getResults() throws IOException {
+    private static String getResults() {
+        final int LIMIT = 100000000;
+        StringJoiner str = new StringJoiner("\n\n");
         int[] inputs = { 2, 3, 4, 523, 67800, LIMIT / 8, LIMIT / 4, LIMIT / 2, LIMIT - 8,
                 LIMIT };
         for (int i : inputs) {
-            Main.input = i;
             for (int j = 0; j < 11; j++) {
-                buf_so.write(("\n in" + input).getBytes());
-                Main.thread_count = 1 << j;
+                str.add("\nin " + i);
+                int count = 1 << j;
                 for (int k = 0; k < 5; k++) {
-                    doTask();
+                    str.add(doTask(i, count));
                 }
-
             }
         }
+        return str.toString();
     }
 
-    private static ExecutorService initPool() {
+    // private static String getResults() throws IOException {
+    // final int LIMIT = 100000000;
+    // String str = "";
+    // int[] inputs = { 2, 3, 4, 523, 67800, LIMIT / 8, LIMIT / 4, LIMIT / 2, LIMIT
+    // - 8,
+    // LIMIT };
+    // for (int i : inputs) {
+    // for (int j = 0; j < 11; j++) {
+    // str.concat("\n in" + i);
+    // int count = 1 << j;
+    // for (int k = 0; k < 5; k++) {
+    // str.concat(doTask(i, count));
+    // }
+    // }
+    // }
 
-        return (Main.thread_count > 1) ? Executors.newFixedThreadPool(thread_count) : null;
+    // return str;
+    // }
+    //
+    private static ExecutorService initPool(int count) {
+
+        return (count > 1) ? Executors.newFixedThreadPool(count) : null;
     }
 
     private static void killPool(ExecutorService es) {
-        if (es != null)
-            es.shutdownNow();
+        if (es == null)
+            return;
+        es.shutdown();
+        try {
+            while (!es.awaitTermination(0, TimeUnit.MICROSECONDS))
+                ;
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
     private static void getter(Future<?> f) {
@@ -135,7 +177,7 @@ public class Main {
         }
     };
 
-    private static void doTask() {
+    private static String doTask(int input, int thread_count) {
         /*
          * List<Integer> primes = new ArrayList<Integer>();
          * 
@@ -169,7 +211,6 @@ public class Main {
          * 
          * LOCK.unlock();
          */
-
         double t0 = System.nanoTime();
         int lim = (int) Math.sqrt(input);
         boolean sieve[] = new boolean[input - 1];
@@ -183,21 +224,20 @@ public class Main {
                 }
             }
         } else {
-            List<Future<?>> blocker = new ArrayList<>();
-            ExecutorService es = initPool();
+            // List<Future<?>> blocker = new ArrayList<>();
+            ExecutorService es = initPool(thread_count);
 
             for (int i = 2; i <= lim; i++) {
                 if (sieve[i - 2])
                     continue;
                 int ind = i;
-
-                blocker.add(
-                        es.submit(() -> {
-                            getMulti(sieve, ind);
-                        }));
+                es.submit(() -> {
+                    getMulti(sieve, ind, input);
+                });
+                // blocker.add( );
             }
 
-            blocker.forEach(Main::getter);
+            // blocker.forEach(Main::getter);
             killPool(es);
         }
 
@@ -207,20 +247,14 @@ public class Main {
             if (!notPrime)
                 n++;
         }
-        double dt = ((float) (System.nanoTime() - t0)) * 0.000001;
-        String str = fString.formatted(n, thread_count, dt);
-        try {
-            buf_so.write(str.getBytes());
 
-        } catch (IOException e) {
-            System.out.println(CYKA);
-            System.err.println(CYKA + " when getting input");
-            e.printStackTrace();
-        }
+        final String fString = "%d primes were found. %d threads took %.3f ms";
+        double dt = ((float) (System.nanoTime() - t0)) * 0.000001;
+        return fString.formatted(n, thread_count, dt);
 
     }
 
-    private static void getMulti(boolean[] arr, Integer ind) {
+    private static void getMulti(boolean[] arr, Integer ind, int input) {
 
         for (long i = ind * ind; i <= input && arr[ind - 2] == false; i += ind) {
 
